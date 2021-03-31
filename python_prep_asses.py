b7a0e545a3057bf9a5749d9d3d07006c41f8b1ee
Olejekglejek <negrutaoleg@gmail.com>
2021-03-31 10:40:02 +0100

    exam prep ex2

---
+++ python_prep_asses.py
@@ -0,0 +1,11 @@
+
+# 1. Create a function called greetings and takes one argument as string and returns the name
+
+# def greetings(arg):
+#     return val
+
+# 2. Declare a list of numbers from 0 to 9, iterate thorugh the list and print the list
+
+list = [0,1,2,3,4,5,6,7,8,9]
+for num in list:
+    print(num)
