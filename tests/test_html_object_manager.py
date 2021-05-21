11116c54d8a143f9c593bf25252b966ebaf24e0b
ArunPanesar42 <ArunSPanesar@gmail.com>
2021-05-21 12:42:35 +0100

    added dockerfile

--- tests/test_html_object_manager.py
+++ tests/test_html_object_manager.py
@@ -1,23 +1,23 @@
 import pytest
 from config_manager import itjobswatch_home_page_test_file, itjobswatch_home_page_url, get_test_env_setting
-from src.http_management.http_manager import HttpManager
+from src.http_management.http_manager import HtmlObjectManager
 
 
-class TestHttpManager:
+class TestHtmlObjectManager:
 
     @pytest.fixture()
     def html_manager_object(self):
         if get_test_env_setting() == 'live':
-            return HttpManager(itjobswatch_home_page_url())
+            return HtmlObjectManager(itjobswatch_home_page_url())
         else:
-            return HttpManager(itjobswatch_home_page_test_file())
+            return HtmlObjectManager(itjobswatch_home_page_test_file())
 
     def test_html_manager_file_is_opened_correctly(self, html_manager_object):
         assert type(html_manager_object.html) is str
 
     def test_incorrect_file_or_url_path_raises_name_error(self):
         with pytest.raises(NameError):
-            assert HttpManager("test")
+            assert HtmlObjectManager("test")
 
     @pytest.mark.skipif(get_test_env_setting() == 'test', reason='This test is specific to a http call and is not valid when testing locally')
     def test_html_manager_status_response_is_200(self, html_manager_object):
